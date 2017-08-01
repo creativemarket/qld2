d6596a38fd680dd696afa75a8d9cfe4754d31f7f
Alana Anderson <alanatreimanis@gmail.com>
2017-08-01 19:42:22 -0400

    Isolate: Remove vue-apollo for the time being to

--- src/main.js
+++ src/main.js
@@ -1,30 +1,57 @@
-import Vue from 'vue';
-import App from './app.vue';
-import { ApolloClient, createNetworkInterface } from 'apollo-client';
-import VueApollo from 'vue-apollo';
+// import Vue from 'vue';
+// import App from './app.vue';
+// import { ApolloClient, createNetworkInterface } from 'apollo-client';
+// import VueApollo from 'vue-apollo';
 
-// Create the Apollo client
-const apolloClient = new ApolloClient({
-    networkInterface: createNetworkInterface({
-        uri: 'http://localhost:8080/graphql',
-        transportBatching: true,
-        opts: {
-            credentials: 'same-origin',
-            mode: 'no-cors',
-        },
-    }),
-    connectToDevTools: true,
+// // Create the Apollo client
+// const apolloClient = new ApolloClient({
+//     networkInterface: createNetworkInterface({
+//         uri: 'http://localhost:8080/graphql',
+//         transportBatching: true,
+//         opts: {
+//             credentials: 'same-origin',
+//             mode: 'no-cors',
+//         },
+//     }),
+//     connectToDevTools: true,
+// });
+
+// const apolloProvider = new VueApollo({
+//     defaultClient: apolloClient,
+// });
+
+// // Install the Vue plugin
+// Vue.use(VueApollo);
+
+// new Vue({
+//     el: '#app',
+//     apolloProvider,
+//     render: h => h(App),
+// });
+
+
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:8080/graphql',
+    transportBatching: true,
+    opts: {
+        credentials: 'same-origin',
+        mode: 'no-cors',
+    },
+  }),
 });
 
-const apolloProvider = new VueApollo({
-    defaultClient: apolloClient,
-});
-
-// Install the Vue plugin
-Vue.use(VueApollo);
-
-new Vue({
-    el: '#app',
-    apolloProvider,
-    render: h => h(App),
-});
+client.query({
+  query: gql`
+    query quiz {
+        quiz {
+            quote {
+                text
+            }
+        }
+    }
+  `,
+}).then(data => console.log('success:', data)).catch(error => console.error('error', error));

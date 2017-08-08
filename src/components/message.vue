e9410509acbe7cce401e06eb9b2be84f8602b0fe
Alana Anderson <alanatreimanis@gmail.com>
2017-08-08 18:07:01 -0400

    Alerts

--- src/components/message.vue
+++ src/components/message.vue
@@ -1,4 +1,6 @@
 <script>
+import { EventBus } from '../main.js';
+
 export default {
     props: {
         message: {
@@ -6,15 +8,21 @@ export default {
             required: true,
         },
     },
+    updated() {
+        setTimeout(() => {
+            EventBus.$emit('show:message', { visibile: false, type: undefined });
+        }, 2000); 
+    },
 }
 </script>
 
 <template>
-    <div>
-        <h3>{{ message.type }}</h3>
+    <div v-if="this.message.type" :class="'alert alert-' + this.message.type + ''">
+        <h4 v-if="this.message.type == 'correct'">Correct! Well done kid!</h4>
+        <h4 v-else>Incorrect! You must learn to use the force...</h4>
     </div>
 </template>
 
 <style lang="scss" scoped>
-   @import "~styles/card.scss";
+   @import "~styles/message.scss";
 </style>
